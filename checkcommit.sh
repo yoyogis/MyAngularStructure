fd50fc90442c00b02670e5e4ba67e4f7a6ca7d2e
袁玉 <yu.yuan@tendcloud.com>
2018-06-15 14:56:54 +0800

    test prepush

--- checkcommit.sh
+++ checkcommit.sh
@@ -1,3 +1,22 @@
-# 在dev里但是不再release
-result=`git cherry -v release dev`
-echo "length=${#result}"
+echo "haha"
+current=`git symbolic-ref --short -q HEAD`
+mm="master"
+if [ $mm = $current ]
+then
+    # 在dev里但是不再release
+    result=`git cherry -v release dev`
+    echo "length=${#result}"
+    if [ -z "$result" ]
+    then
+        echo "pass"
+        exit 0
+    else
+        exit 1
+    fi
+else
+    echo "pass all"
+    exit 0 
+fi
+exit 1
+
+DOC_END
